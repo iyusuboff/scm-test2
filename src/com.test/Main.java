05644fc55b65f68484503ac6795afdc51ca0865f
i.yusubov555@gmail.com <i.yusubov555@gmail.com>
2019-06-19 17:47:23 +0400

    Main file test added

--- src/com.test/Main.java
+++ src/com.test/Main.java
@@ -3,6 +3,11 @@ package com.test;
 public class Main{
 
     public static void main(String[] args){
+        Main main=new Main();
+        main.test();
+    }
 
+    private void test(){
+        System.out.print("Hello");
     }
 }
