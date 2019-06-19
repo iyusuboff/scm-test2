2c507e0848f76935eb0735666cdf0b3a2df34504
i.yusubov555@gmail.com <i.yusubov555@gmail.com>
2019-06-19 17:47:49 +0400

    Main file test2 added

--- src/com.test/Main.java
+++ src/com.test/Main.java
@@ -5,9 +5,14 @@ public class Main{
     public static void main(String[] args){
         Main main=new Main();
         main.test();
+        main.test2();
     }
 
     private void test(){
+        System.out.print("Hello");
+    }
+
+    private void test2(){
         System.out.print("Hello");
     }
 }
